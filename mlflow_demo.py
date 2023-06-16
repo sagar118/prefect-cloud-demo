beb9162944ee1d0368b00c918e9fbb23b01d155f
Sagar Thacker <sgrhacker18@gmail.com>
2023-06-16 10:22:39 -0400

    Add mlflow demo

---
+++ mlflow_demo.py
@@ -0,0 +1,19 @@
+import mlflow
+from prefect import flow, task
+
+@task(name="mlflow-run", log_prints=True)
+def mlflow_run():
+  with mlflow.start_run():
+    mlflow.log_metric("metric", 1.0)
+
+@flow(name="mlflow-flow", log_prints=True)
+def flow_hello():
+    print("Hello, I'm a Flow!")
+    print("I'm about to call a Task...")
+    mlflow_run()
+
+if __name__ == "__main__":
+    mlflow.set_tracking_uri("http://localhost:5000")
+    mlflow.set_experiment("demo")
+
+    flow_hello()
